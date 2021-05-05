0187192389f93dd58263266bf22e356face19414
Mark Callow <github@callow.im>
2021-05-05 15:29:50 +0900

    Add info about dfdutils, format mappings and case generator.

--- index.php
+++ index.php
@@ -12,12 +12,22 @@ include_once("../../assets/static_pages/khr_page_top.php");
     backing store</a> repo on GitHub.</p>
 
 <p> There is currently no header file or other canonical machine readable description
-    for either KTX v1 or v2.
+    for either KTX v1 or v2.</p>
  
-<p> Implementers of readers and writers may find the header file
-    <a href="../dataformat/api/1.3/khr_df.h">khr_df.h</a> in the
-    <a href="../dataformat">Khronos Data Format Specification Registry</a> useful
-    for dealing with the Data Format Descriptor part of KTX v2.0.
+<p> Implementers of readers and writers may find the following useful:</p>
+<ul>
+<li>The header file <a href="../dataformat/api/1.3/khr_df.h">khr_df.h</a> in the
+    <a href="../dataformat">Khronos Data Format Specification Registry</a>
+    for dealing with the Data Format Descriptor part of KTX v2.0.</li>
+<li>The <a href="">Data Format Utilities</a> also for dealing with the Data
+    Format Descriptor.</li>
+<li>The <a href="https://github.com/KhronosGroup/KTX-Specification/blob/master/formats.json">
+    JSON database (schema)</a> with mappings from Vulkan format enums to OpenGL, Direct3D
+    and Metal enums.</li>
+<li>The <a href="https://github.com/KhronosGroup/KTX-Specification/blob/master/generate_format_switches.rb">
+    Switch-case generator</a> that produces 5 files with simple C-like case-return
+    statements from the above mappings.</li>
+</ul>
 
 <h4> KTX File Format Specifications</h4>
 
